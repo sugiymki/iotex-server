976cced06115b14b8826992659cc9ad25721f5ac
root <root@akatsuki.epi.it.matsue-ct.jp>
2021-10-05 00:03:32 +0900

    Update

--- sample/schema.sql
+++ sample/schema.sql
@@ -1,25 +1,19 @@
--- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.18  Distrib 10.3.27-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: iotex
 -- ------------------------------------------------------
--- Server version	10.1.26-MariaDB-0+deb9u1
+-- Server version	10.3.27-MariaDB-0+deb10u1
 
-
---
--- Table structure for table `monitoring_hosts`
---
-
-DROP TABLE IF EXISTS `monitoring_hosts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `monitoring_hosts` (
-  `hostname` varchar(20) DEFAULT NULL,
-  `ip` varchar(15) DEFAULT NULL,
-  `time` datetime DEFAULT NULL,
-  PRIMARY KEY (`hostname`)  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `monitoring`
@@ -29,39 +23,7 @@ DROP TABLE IF EXISTS `monitoring`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `monitoring` (
-  `hostname` varchar(20) DEFAULT NULL,
-  `time` datetime DEFAULT NULL,
-  `temp` double DEFAULT NULL,
-  `temp2` double DEFAULT NULL,
-  `temp3` double DEFAULT NULL,
-  `humi` double DEFAULT NULL,
-  `humi2` double DEFAULT NULL,
-  `humi3` double DEFAULT NULL,
-  `dp` double DEFAULT NULL,
-  `dp2` double DEFAULT NULL,
-  `dp3` double DEFAULT NULL,
-  `bmptemp` double DEFAULT NULL,
-  `dietemp` double DEFAULT NULL,
-  `lux` double DEFAULT NULL,
-  `objtemp` double DEFAULT NULL,
-  `pres` double DEFAULT NULL,
-  `sitemp` double DEFAULT NULL,
-  `sihumi` double DEFAULT NULL,
-  `eco2` double DEFAULT NULL,
-  `tvoc` double DEFAULT NULL,
-  PRIMARY KEY (`hostname`,`time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `monitoring_10min`
---
-
-DROP TABLE IF EXISTS `monitoring_10min`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `monitoring_10min` (
-  `hostname` varchar(20) NOT NULL,
+  `hostname` varchar(20) CHARACTER SET utf8 NOT NULL,
   `time` datetime NOT NULL,
   `temp` double DEFAULT NULL,
   `temp2` double DEFAULT NULL,
@@ -72,39 +34,42 @@ CREATE TABLE `monitoring_10min` (
   `dp` double DEFAULT NULL,
   `dp2` double DEFAULT NULL,
   `dp3` double DEFAULT NULL,
-  `pres` double DEFAULT NULL,
   `bmptemp` double DEFAULT NULL,
   `dietemp` double DEFAULT NULL,
+  `lux` double DEFAULT NULL,
   `objtemp` double DEFAULT NULL,
+  `pres` double DEFAULT NULL,
   `sitemp` double DEFAULT NULL,
   `sihumi` double DEFAULT NULL,
   `eco2` double DEFAULT NULL,
   `tvoc` double DEFAULT NULL,
-  `lux` double DEFAULT NULL,
-  `didx` double DEFAULT NULL,
-  `didx2` double DEFAULT NULL,
-  `didx3` double DEFAULT NULL,
-  `sididx` double DEFAULT NULL,  
-  PRIMARY KEY (`hostname`,`time`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `winddir` double DEFAULT NULL,
+  `windvel` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
-
 --
--- Table structure for table `monitoring_1hour` etc.
+-- Table structure for table `monitoring_hosts`
 --
 
-DROP TABLE IF EXISTS `monitoring_1hour`;
-CREATE TABLE `monitoring_1hour` LIKE `monitoring_10min`;
+DROP TABLE IF EXISTS `monitoring_hosts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `monitoring_hosts` (
+  `hostname` varchar(20) NOT NULL,
+  `ip` varchar(15) DEFAULT NULL,
+  `time` datetime DEFAULT NULL,
+  PRIMARY KEY (`hostname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-DROP TABLE IF EXISTS `monitoring_1day_min`;
-CREATE TABLE `monitoring_1day_min` LIKE `monitoring_10min`;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-DROP TABLE IF EXISTS `monitoring_1day_max`;
-CREATE TABLE `monitoring_1day_max` LIKE `monitoring_10min`;
-
-DROP TABLE IF EXISTS `monitoring_1day_avg`;
-CREATE TABLE `monitoring_1day_avg` LIKE `monitoring_10min`;
-
-DROP TABLE IF EXISTS `monitoring_1day_stddev`;
-CREATE TABLE `monitoring_1day_stddev` LIKE `monitoring_10min`;
+-- Dump completed on 2021-10-04 23:59:03
